9f67d29854a210daf6e425a94458a6b5db857689
wuthmone <whlaing10@gmail.com>
2017-01-19 15:34:45 +0800

    Added new files

--- myApp/ui.R
+++ myApp/ui.R
@@ -13,21 +13,20 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Data Science"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+       h1("Move the slider!"),
+       sliderInput("slider2","Slide me!", 0,100,0)
+      
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+       h3("Slider Value"),
+        textOutput("text1")
     )
   )
 ))
